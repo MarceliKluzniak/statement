cda3a03cc887f125ab1f81e70d5d488186d6a34d
Marceli Kluzniak <marcelikluzniak5@gmail.com>
2021-04-27 19:26:01 +0200

    sample of statement

---
+++ src/pl/mkluzniak/Main.java
@@ -0,0 +1,25 @@
+package pl.mkluzniak;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int age = 18;
+        if(age>=18){
+            System.out.println("You are adult!");
+        }
+        else if (age>=75){
+            System.out.println("Ok Boomer!");
+        }
+        else if (age>=13){
+            System.out.println("You are a teeneger!");
+        }
+        else {
+            System.out.println("You are not an adult!");
+        }
+
+
+
+
+    }
+}
